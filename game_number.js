aea60cb7d5aabf8aadb2f2da3474956dc106156a
bushlalex <bushlya199920122@gmail.com>
2025-01-06 21:45:16 +0500

    дописал игру, осталась стилизация

--- game_number.js
+++ game_number.js
@@ -1,33 +1,74 @@
-//Сделать функцию, вывод которой будет рандомным числом от 1 до 100 (Исключить 0)
 
-function randomNmbr () {
-    let x = Math.floor(Math.random(1)*100);
-    if (x==0) {return x+1}
-    else return x
-};
-randomNmbr ();
-let x = randomNmbr();
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+let schetchikPopitok = 0;
 
-//Сделать функцию, которая берет число от пользователя//
 
-let z = onclick.button;
-
-/*Сделать функцию, которая принимает число от пользователя, сравнивает это число с числом возврата 
-функции randomNmbr() и выводит дальнейшие решения*/
-
-function userNumber (numberUser) {
-    if (numberUser == x) {
-        return console.log("Вы угадали это число!")
-    }
-    else if (numberUser > x) {
-        return console.log("Ваше число больше загаданного числа!")
-    }
-    else if (numberUser < x) {
-        return console.log("Ваше число меньше загаданного числа!")}
-
-    else return console.log("Введите число")
+function ugadat () {
+  return Number(document.querySelector('#predpolozenie').value) 
 }
-userNumber(12);
+
+function sravnenie (x, y) {
+  if (x==y) {
+    document.getElementById('gameover').innerHTML = "Вы угадали это число!"
+    document.getElementById('popitki').innerHTML = "Попыток осталось: " + (9 - schetchikPopitok);
+    const button = document.getElementById("ugadat");
+    button.style.display = "none"
+    ++schetchikPopitok;
+    return  "Это число " + y + " !"
+  }
+  else if (x>y) {
+    document.getElementById('gameover').innerHTML = "Число " + y + " меньше загаданного!"
+    document.getElementById('popitki').innerHTML = "Попыток осталось: " + (9 - schetchikPopitok);
+    ++schetchikPopitok;
+    return    y + " < Загаданное число"
+  }
+  else if (x<y) {
+    document.getElementById('gameover').innerHTML = "Число " + y + " больше загаданного!"
+    document.getElementById('popitki').innerHTML = "Попыток осталось: " + (9 - schetchikPopitok);
+    ++schetchikPopitok;
+    return  y + " > Загаданное число"
+  }  
+}
+
+function raspechatatRezultatRaunda () {
+  let parent = document.querySelector('#rezultat');
+  let p = document.createElement('p');
+  p.textContent = sravnenie (randomNumber, ugadat ());
+  parent.appendChild(p);     
+}
+
+function obnovitStranicu () {
+  location.reload();
+}
+
+function igraVNeskolkoPopitok (osnovaFunction, schetchik) {
+  if ((schetchik == 10)&&(randomNumber != ugadat ())) {
+  const button = document.getElementById("ugadat");
+  button.style.display = "none"
+  document.getElementById('gameover').innerHTML = "Вы проиграли! Загаданное число было: " + randomNumber;
+  }
+  else if ((schetchik == 10)&&(randomNumber == ugadat ())) {
+  document.getElementById('gameover').innerHTML = "Вы угадали это число!"}
+  else osnovaFunction;  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
